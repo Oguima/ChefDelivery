843fd169feb9c5ac6411d1fe631c42774dc734fe
oguimacit <rafael.guimaraes@ciandt.com>
2023-08-07 21:40:25 -0300

    Detail part 01

--- ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -11,17 +11,51 @@ struct StoreDetailView: View {
     let store: StoreType
     
     var body: some View {
-        VStack {
-            Text("StoredDetailView Screem")
-            Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            HStack {
+                Text(store.name)
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Image(store.logoImage)
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            HStack {
+                Text(store.location)
+                
+                Spacer()
+                
+                //id: \.self (Para identificar cada elemento...
+                ForEach(1...store.stars, id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .font(.caption)
+                }
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            Text("Produtos")
+                .font(.title2)
+                .bold()
+                .padding()
         }
+        .navigationTitle(store.name) //Título da barra de navegacao...
+        //.navigationBarTitleDisplayMode(.large)
+        .navigationBarTitleDisplayMode(.inline)
     }
+        
+        
 }
 
 struct StoreDetailView_Previews: PreviewProvider {
     static var previews: some View {
         StoreDetailView(store: storesMock[0])
             .previewLayout(.sizeThatFits)
-            .padding()
     }
 }
